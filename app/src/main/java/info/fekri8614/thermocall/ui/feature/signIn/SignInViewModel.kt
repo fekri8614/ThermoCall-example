047d367d249af0591e833561306f797a40a40eae
Mohammad Reza Fekri <92860582+fekri8614@users.noreply.github.com>
2024-03-18 19:05:36 +0330

    SignIn navigation is completed

--- app/src/main/java/info/fekri8614/thermocall/ui/feature/signIn/SignInViewModel.kt
+++ app/src/main/java/info/fekri8614/thermocall/ui/feature/signIn/SignInViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import info.fekri8614.thermocall.model.repository.user.UserRepository
 import info.fekri8614.thermocall.util.coroutineExceptionHandler
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.seconds
 
 class SignInViewModel(private val userRepository: UserRepository) : ViewModel() {
     val email = MutableLiveData("")
@@ -16,6 +18,10 @@ class SignInViewModel(private val userRepository: UserRepository) : ViewModel() 
         viewModelScope.launch(coroutineExceptionHandler) {
             // val result = userRepository.signIn(email.value!!, password.value!!)
             //LoggingEvent(result)
+
+            delay(1.4.seconds)
+
+            loggingEvent("success")
 
             print("USER_DATA = $loggingEvent")
         }
